792612b6bc8d31d55bcd72853a487dcca0f1ea13
John Shea <john@johndshea.com>
2015-09-06 09:50:21 -0400

    added parentheses to getPlayerMove and getComputerMove if statements

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,7 +25,7 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    if move {
+    if (move) {
         return move;
     }
     else {
@@ -37,7 +37,7 @@ function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    if move {
+    if (move) {
         return move;
     }
     else {
